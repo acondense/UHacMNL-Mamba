a2ac2f77d34911d8b665f45481f7f1013477c258
acondense <ariel.conde1997@gmail.com>
2016-08-28 12:18:59 +0800

    Added post route

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/',function(req,res){
   res.render(path.join(__dirname+'/views/goal/index.html'));
 });
 
-app.get('/dashboard',function(req,res){
+app.get('/startup/dashboard',function(req,res){
   res.render(path.join(__dirname+'/views/dashboard/index.html'));
 });
 
@@ -24,7 +24,53 @@ app.get('/goal',function(req,res){
 });
 
 app.get('/loan-page',function(req,res){
-  res.render(path.join(__dirname+'/views/goal/index.html'));
+  res.render(path.join(__dirname+'/views/loan-page/index.html'));
+});
+
+app.get('/postroute',function(req,res){
+  // res.render(path.join(__dirname+'/views/loan-page/index.html'));
+  
+          var request = require("request");
+
+        var options = { method: 'POST',
+          url: 'https://api.us.apiconnect.ibmcloud.com/ubpapi-dev/sb/api/RESTs/transfer',
+          headers: 
+           { accept: 'application/json',
+             'content-type': 'application/json',
+             'x-ibm-client-secret': 'E5fE3jP2tY2vC6yU0lA0dK4yE7vL4tH3jU4gC2sF0yB2jI1uL6',
+              'x-ibm-client-id': 'b4e5be45-2893-4766-88a2-f90454cf56e1'  },
+          body: req.query,
+          json: true };
+
+        request(options, function (error, response, body) {
+          if (error) return console.error('Failed: %s', error.message);
+          res.send(body);
+
+          // console.log('Success: ', body);
+        });
+});
+
+app.get('/loanroute',function(req,res){
+  // res.render(path.join(__dirname+'/views/loan-page/index.html'));
+  
+          var request = require("request");
+
+        var options = { method: 'POST',
+          url: 'https://api.us.apiconnect.ibmcloud.com/ubpapi-dev/sb/api/RESTs/transfer',
+          headers: 
+           { accept: 'application/json',
+             'content-type': 'application/json',
+             'x-ibm-client-secret': 'E5fE3jP2tY2vC6yU0lA0dK4yE7vL4tH3jU4gC2sF0yB2jI1uL6',
+              'x-ibm-client-id': 'b4e5be45-2893-4766-88a2-f90454cf56e1'  },
+          body: req.query,
+          json: true };
+
+        request(options, function (error, response, body) {
+          if (error) return console.error('Failed: %s', error.message);
+          res.send(body);
+
+          // console.log('Success: ', body);
+        });
 });
 
 require("cf-deployment-tracker-client").track();
